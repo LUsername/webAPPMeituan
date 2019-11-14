930b4a5ea7c3d16717cea092cded4edc8de972ce
LUsername <492254446@qq.com>
2019-11-14 15:35:13 +0800

    详情页-左侧类目数据渲染

--- menu/left/left.js
+++ menu/left/left.js
@@ -10,7 +10,32 @@
     function getList() {
         $.get('../json/food.json', function(data) {
             console.log(data);
+            var list = data.data.food_spu_tags || [];
+            initContentList(list);
         })
+    }
+    /**
+     * 渲染item内容
+     * @param obj
+     */
+    function getItemContent(data) {
+        if (data.icon) {
+            return '<img class="item-icon" src=' + data.icon + ' />' + data.name;
+        } else {
+            return data.name;
+        }
+    }
+    /**
+     * 渲染列表
+     * @param array
+     */
+    function initContentList(list) {
+        list.forEach(function(item, index) {
+            var str = itemTmpl.replace('$getItemContent', getItemContent(item));
+            var $target = $(str);
+            $target.data('itemData', item);
+            $('.left-bar-inner').append($target);
+        });
     }
 
     function init() {
