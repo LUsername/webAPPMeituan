673e184f1f4b237b90dc34e15bf604597a96acd7
LUsername <492254446@qq.com>
2019-11-14 17:46:58 +0800

    购物车（1）

---
+++ menu/shopBar/shopBar.js
@@ -0,0 +1,14 @@
+(function() {
+    // 顶部模板字符串
+    var itemTopTmpl = '<div class="choose-content hide">' +
+        '<div class="content-top">' +
+        '<div class="clear-car">清空购物车</div>' +
+        '</div>' +
+        '</div>';
+    // 底部模板字符串
+    var itemBottomTmpl = '<div class="bottom-content">' +
+        '<div class="shop-icon">' +
+        '<div class="dot-num hide"></div>' +
+        '</div>' +
+        '</div>';
+})();
